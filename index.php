28d1a3f9815c9d26e67974b62335ba1be6cf6a04
Davi Tavares <dvt.siqueira@gmail.com>
2026-02-05 08:20:18 -0300

    meu primeiro commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Minha Loja Virtual</title>
+</head>
+<body>
+
+    <h1>Minha Loja Virtual</h1>
+
+    <p>
+        <?php
+            echo "Bem-vindo à nossa loja!";
+        ?>
+    </p>
+
+    <p>
+    Acesso realizado às:
+    <?php echo date("H:i:s"); ?>
+</p>
+
+
+<form method="get">
+    <input type="text" name="nome">
+    <button type="submit">Entrar</button>
+</form>
+
+<?php
+    if (isset($_GET["nome"])) {
+        echo "Olá, " . $_GET["nome"] . "! Seja bem-vindo!";
+    }
+?>
+
+</body>
+</html>
