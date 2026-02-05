cf00ddfebdc78f2b3079a506f2fe26f058ea8d07
Davi Tavares <dvt.siqueira@gmail.com>
2026-02-05 09:55:44 -0300

    desafio

---
+++ desafio.php
@@ -0,0 +1,15 @@
+<?php
+$nomeAluno = 'Davi';
+$idadeAluno = 40;
+$curso = 'Programação para Internet III';
+
+echo ("Meu nome é ". $nomeAluno . ",
+tenho " . $idadeAluno . " anos e estou no curso de " .
+$curso . ".");
+
+$nota1=8;
+$nota2=5;
+$media=($nota1+$nota2)/2;
+echo ("<br/>A média das notas é: <b>" . $media . "</b>");
+
+?>
