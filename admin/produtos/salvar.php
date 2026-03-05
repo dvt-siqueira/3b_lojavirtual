5337ea79f885bec8ac1b3c5f487c01b521c0d438
Davi Tavares <dvt.siqueira@gmail.com>
2026-03-05 07:57:48 -0300

    exercicios revisao

--- admin/produtos/salvar.php
+++ admin/produtos/salvar.php
@@ -1,24 +1,43 @@
-<html>
-    <head>
-        <title>Produto Salvo</title>
-         <link rel="stylesheet" href="../../css/styles.css">
-    </head>
-    <body>
-        <div class="container">
-        <h1 id="sucesso">Prouto salvo com Sucesso</h1>
-        <?php
-            if($_SERVER["REQUEST_METHOD"]=="POST"){
-                $nome = $_POST["nome"];
-                $preco = $_POST["preco"];
-                $descricao = $_POST["descricao"];
+<?php
+require_once '../../config.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $preco = $_POST['preco'];
+    //preparar a comando SQL para inserir o produto
+    $sql = "INSERT INTO produtos (nome, descricao, preco)
+ VALUES (:nome, :descricao, :preco)";
+    $stmt = $pdo->prepare($sql);
+    try {
+        $stmt->execute([
+            'nome' => $nome,
+            'descricao' => $descricao,
+            'preco' => $preco
+        ]);
 
-                echo "<p><Strong>Nome:</strong>".htmlspecialchars($nome)."</p>";
-                echo "<p><strong>Preço:</strong> R$ " . number_format($preco, 2, ',', '.') . "</p>";
-                echo "<p><Strong>Descrição:</strong>".htmlspecialchars($descricao)."</p>";
-
-
-            }
+        $mensagem = "Produto salvo com sucesso!";
+    } catch (PDOException $e) {
+        $mensagem = "Erro ao salvar o produto: " . $e->getMessage();
+    }
+}
 ?>
-        </div>
-    </body>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produto Cadastrado </title>
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Produto Cadastrado</h1>
+        <p><?php echo $mensagem; ?></p>
+        <a href="listar.php">Voltar para a lista de produtos</a>
+        <a href="cadastrar.php">Cadastrar outro produto</a>
+    </div>
+</body>
+
 </html>
---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// Database configuration
+$host = 'localhost'; // Database host
+$db = '3b_lojavirtual'; // Database
+$user = 'root'; // Database username
+$pass = ''; // Database password
+$charset = 'utf8mb4'; // Database charset
+ 
+// Data Source Name
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";  
+
+$option = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
+
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $option);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
